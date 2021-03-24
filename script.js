866a7289a91c1c02a75cf1861c79049d13a2d30f
Tairah <Tairah.akintoye@ocsbstudent.ca>
2021-03-24 20:43:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('button').addEventListener('click', randomizer)
+let randomNumber = 0
+let userGuess = 0
+ 
+function randomizer () {
+ userGuess = document.getElementById('input').value
+ userGuess = parseInt(userGuess)
+ 
+ randomNumber = (Math.random() * 6) + 1
+ randomNumber = parseInt(randomNumber)
+ 
+ if (userGuess == randomNumber)
+   document.getElementById('answer').innerHTML = "You win!"
+}
